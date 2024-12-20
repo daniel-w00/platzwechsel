124ec0bf7c6163f2a14bdea1ec81f3dc17d01115
daniel <mail>
2024-12-21 00:17:46 +0100

    matrix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from main import SeatingOptimizer  # Ihr bestehender Code
+from main import SeatingOptimizer 
 
 app = Flask(__name__)
 
@@ -38,4 +38,4 @@ def optimize():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run( host='0.0.0.0')
